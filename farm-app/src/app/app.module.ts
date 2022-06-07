eb3e2bbac60d26c8cadc8a297c359f002e031246
patriciaturato <patricia.turato@aegro.com.br>
2022-06-07 11:18:59 -0300

    add productions components

--- farm-app/src/app/app.module.ts
+++ farm-app/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FarmsComponent } from './farms/farms.component';
 import { FarmListComponent } from './farms/farm-list/farm-list.component';
 import { PlotsComponent } from './plots/plots.component';
 import { PlotListComponent } from './plots/plot-list/plot-list.component';
+import { ProductionsComponent } from './productions/productions.component';
+import { ProductionListComponent } from './productions/production-list/production-list.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { PlotListComponent } from './plots/plot-list/plot-list.component';
     FarmsComponent,
     FarmListComponent,
     PlotsComponent,
-    PlotListComponent
+    PlotListComponent,
+    ProductionsComponent,
+    ProductionListComponent
   ],
   imports: [
     BrowserModule
---
+++ farm-app/src/app/productions/production-list/production-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-production-list',
+  templateUrl: './production-list.component.html',
+  styleUrls: ['./production-list.component.css']
+})
+export class ProductionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

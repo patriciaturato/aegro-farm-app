dbafa93de80263b3fb8fb30e3841cdcdbdc0a10c
patriciaturato <patricia.turato@aegro.com.br>
2022-06-07 11:27:47 -0300

    add component to create a plot

---
+++ farm-app/src/app/plots/add-plot/add-plot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-plot',
+  templateUrl: './add-plot.component.html',
+  styleUrls: ['./add-plot.component.css']
+})
+export class AddPlotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
